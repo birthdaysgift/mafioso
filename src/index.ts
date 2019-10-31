039d92c50e1717803012b42bfa422f66b8ede302
birthdaysgift <most-208@yandex.ru>
2019-10-31 16:09:07 +0300

    Added frontend for main page

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile('index.html'));
 app.listen(port, () => console.log(`App listening on port ${port}!`));
