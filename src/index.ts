0e820d11561664562a25611fbad1a13c8535bc09
birthdaysgift <most-208@yandex.ru>
2019-10-31 20:22:59 +0300

    Added sockets

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import http from 'http'
+import express from 'express'
+import socketio from 'socket.io'
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = 3000;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile('index.html'));
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+io.on('connection', (socket) => {
+    console.log('Somebody connected');
+
+    socket.on('disconnect', () => {
+        console.log('Somebody disconnected');
+    })
+})
+
+server.listen(port, () => {
+    console.log('Server is listening on 3000');
+});
