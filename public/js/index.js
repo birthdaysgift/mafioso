0377b924c3c88d96850bada9df27cb3c529fde7b
birthdaysgift <most-208@yandex.ru>
2019-11-02 22:32:48 +0300

    Front: Added removing form and greeting a new player on 'Start Game'

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,12 @@
+var d = document;
 var socket = io();
+
+var btn = d.querySelector('.StartButton');
+btn.addEventListener('click', (e) => {
+    d.querySelectorAll(
+        '.NameInputFieldText, form, .StartButton, .RulesButton'
+    ).forEach((elem) => elem.remove());
+    d.querySelector('.Background').insertAdjacentHTML(
+        'afterbegin', '<h1 style="color:#fff;">Hello, New Player!</h1>'
+    );
+});
