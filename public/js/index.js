0556ac815fdeb431a041c4b940de1b94bd888fb6
birthdaysgift <most-208@yandex.ru>
2019-11-02 23:03:52 +0300

    Added players creation and disconnection logic

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,20 @@
 var d = document;
 var socket = io();
+var player;
 
 var btn = d.querySelector('.StartButton');
 btn.addEventListener('click', (e) => {
-    d.querySelectorAll(
-        '.NameInputFieldText, form, .StartButton, .RulesButton'
-    ).forEach((elem) => elem.remove());
-    d.querySelector('.Background').insertAdjacentHTML(
-        'afterbegin', '<h1 style="color:#fff;">Hello, New Player!</h1>'
-    );
+    let name = d.querySelector('.NameInputField').value;
+    socket.emit('create player', name);
+    socket.on('created player', (playerJSON) => {
+        player = JSON.parse(playerJSON);
+        
+        d.querySelectorAll(
+            '.NameInputFieldText, form, .StartButton, .RulesButton'
+        ).forEach((elem) => elem.remove());
+        d.querySelector('.Background').insertAdjacentHTML(
+            'afterbegin', `<h1 style="color:#fff;">Hello, ${player.name}!</h1>`
+        );
+    });
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import http from 'http'
 import express from 'express'
 import socketio from 'socket.io'
+import { Socket } from 'net';
 
 const app = express();
 const server = http.createServer(app);
@@ -8,15 +9,41 @@ const io = socketio(server);
 
 const port = 3000;
 
+var players: Player[] = [];
+
+class Player {
+    static nextId: number = 0;
+
+    id: number;
+    name: string;
+
+    constructor(name: string) {
+        this.id = Player.nextId++;
+        this.name = name;
+    }
+
+    toString() {
+        return `${this.id} ${this.name}`;
+    }
+}
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile('index.html'));
 
 io.on('connection', (socket) => {
-    console.log('Somebody connected');
+    socket.on('create player', (playerName) => {
+        let p = new Player(playerName);
+        players.push(p);
+        console.log(`${p} connected`);
+        console.log(`Players: ${players}`);
+        socket.emit('created player', JSON.stringify(p));
 
-    socket.on('disconnect', () => {
-        console.log('Somebody disconnected');
+        socket.on('disconnect', () => {
+            players.splice(players.indexOf(p), 1);
+            console.log(`${p} disconnected`);
+            console.log(`Players: ${players}`);
+        })
     })
 })
 
