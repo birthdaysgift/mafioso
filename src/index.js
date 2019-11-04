3f0dadb728bdef64f4c7a570dcfb80a6bdc9a350
birthdaysgift <most-208@yandex.ru>
2019-11-04 21:57:09 +0300

    Added Pug template engine

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ const server = http.createServer(app);
 const PORT = 3000;
 const STATIC_DIR = path.join(__dirname, '..', '/public');
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.use(express.static(STATIC_DIR, {index: false}));
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: STATIC_DIR});
-});
+app.get('/', (req, res) => res.render('index'));
 
 server.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
