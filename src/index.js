e3d97d98ff1f41580f5832bc4441cde20e61fe22
birthdaysgift <most-208@yandex.ru>
2019-11-05 15:58:19 +0300

    Added simple backend logic for 'Create Game' and 'Join Game'

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(STATIC_DIR, {index: false}));
 app.use(session({secret: 'keyboard cat'}));
 
+var games = new Map(); 
+var nextGameId = 0;
+
 app.route('/')
     .get((req, res) => res.render('index'))
     .post((req, res) => {
@@ -32,16 +35,27 @@ app.route('/new_game/')
         res.render('new_game', {name: name});
     });
 app.post('/create_game/', (req, res) => {
-    console.log('Create');
-    console.log(JSON.stringify(req.body));
-    res.redirect('/lobby/');
+    let game = {
+        "id": nextGameId++,
+        "title": req.body.title,
+        "host": req.session.user,
+        "members": [req.session.user]
+    }
+    games.set(game.id, game);
+    req.session.game = game;
+    res.redirect('/game/');
 });
 app.post('/join_game/', (req, res) => {
-    console.log('Join');
-    console.log(JSON.stringify(req.body));
-    res.redirect('/lobby/');
+    let game = games.get(parseInt(req.body.id));
+    game.members.push(req.session.user);
+    req.session.game = game;
+    res.redirect('/game/');
 })
-app.get('/lobby/', (req, res) => res.send('LOBBY'));
+app.get('/game/', (req, res) => {
+    console.log(JSON.stringify(req.session.user));
+    console.log(JSON.stringify(req.session.game));
+    res.send('GAME');
+});
 
 server.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
