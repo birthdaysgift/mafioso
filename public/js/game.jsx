d9ba6c05feff8e1a1f36b396ebd49a1a4054d26a
birthdaysgift <most-208@yandex.ru>
2019-11-07 19:30:58 +0300

    Added ReactJS for 'READY' and 'START' buttons

---
+++ public/js/game.jsx
@@ -0,0 +1,95 @@
+var d = document;
+var userId;
+var gameId;
+var socket;
+
+axios.get('/whatishappening/')
+    .then((response) => {
+        userId = response.data.userId;
+        gameId = response.data.gameId;
+        socket = io(`/${gameId}-game`);
+        socket.on('new member', (userJSON) => {
+            let u = JSON.parse(userJSON);
+            d.querySelector('ul').insertAdjacentHTML(
+                'beforeend', `<li>${u.name}</li>`
+            );
+        });
+        socket.on('user ready', (userJSON) => {
+            let u = JSON.parse(userJSON);
+            d.querySelector('ul').insertAdjacentHTML(
+                'afterend', `<div>${u.name} READY`
+            );
+        });
+    });
+
+class StartButton extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        console.log('start clicked');
+    }
+
+    render() {
+        let element = <div onClick={this.handleClick}>START</div>;
+        return this.props.showStart ? element : null
+    }
+}
+
+class ReadyButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {showReady: true};
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        let data = {userId: userId, gameId: gameId}
+        if (this.state.showReady) {
+            socket.emit('user ready', data);
+        }
+        this.setState((state) => ({showReady: !state.showReady}));
+        this.props.onReadyClick(e);
+    }
+
+    render() {
+        return (
+            <div onClick={this.handleClick}>
+                {this.state.showReady ? "READY" : "NOT READY"}
+            </div>
+        )
+    }
+}
+
+class Buttons extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {showStart: false, showReady: true};
+
+        this.handleReadyClick = this.handleReadyClick.bind(this);
+    }
+
+    handleReadyClick(e) {
+        socket.on('everybody ready', () => {
+            this.setState({showStart: true});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <StartButton showStart={this.state.showStart} />
+                <ReadyButton 
+                    onReadyClick={this.handleReadyClick} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Buttons />, d.querySelector('.buttons')
+);
