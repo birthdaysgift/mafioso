0055d0ee4ecef77cfd14635ac2a9c526468bbfe1
birthdaysgift <most-208@yandex.ru>
2021-01-15 19:26:53 +0300

    Move 'About' button from Login to WelcomePage

--- client/src/Login/index.jsx
+++ client/src/Login/index.jsx
@@ -7,14 +7,11 @@ import PageContext from '../context';
 import './style.less';
 
 export default class Login extends Component {
-    static contextType = PageContext;
-
     render() {
         return (
             <form id='login'>
                 <Input placeholder='Enter your name'/>
                 <Button text='Start'/>
-                <Button text='About' onClick={() => this.context.setRoute('/about')}/>
             </form>
         )
     }
--- client/src/WelcomePage/index.jsx
+++ client/src/WelcomePage/index.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+import Button from '../Button';
+import PageContext from '../context';
 import Login from '../Login';
 import Title from '../Title';
 
 import './style.less';
 
-export default function WelcomePage(props) {
-    return (
-        <div id='welcome_page'>
-            <Title/>
-            <Login/>
-        </div>
-    )
+export default class WelcomePage extends Component {
+    static contextType = PageContext;
+    render () {
+        return (
+            <div id='welcome_page'>
+                <Title/>
+                <Login/>
+                <Button text='About' onClick={() => this.context.setRoute('/about')}/>
+            </div>
+        )
+    }
 } 
