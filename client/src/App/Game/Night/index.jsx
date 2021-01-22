978108e8a5d89c0c40d41639ca561d662d7779b6
birthdaysgift <most-208@yandex.ru>
2021-01-22 21:28:14 +0300

    Add countdown before Night

--- client/src/App/Game/Night/index.jsx
+++ client/src/App/Game/Night/index.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
 export default class Night extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {counter: 3};
+        this.intervalID = setInterval(this.decrCounter, 1000);
+        this.decrCounter = this.decrCounter.bind(this);
+    }
+
+    componentWillUnmount = () => clearInterval(this.intervalID);
+
+    decrCounter = () => {
+        if (this.state.counter === 0) {
+            clearInterval(this.intervalID);
+        } else {
+            this.setState((state,props) => {
+                return { counter: --state.counter };
+            });
+        }
+    }
     render() {
         return (
-            <h1>Night</h1>
+            <h1>{this.state.counter > 0 ? this.state.counter : 'Night'}</h1>
         )
     }
 }
