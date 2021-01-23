421e903ca124a158da9dfd6d76fd65e5ca751444
birthdaysgift <most-208@yandex.ru>
2021-01-23 15:11:23 +0300

    Add style to Mafia component

--- client/src/App/Game/Mafia/index.jsx
+++ client/src/App/Game/Mafia/index.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
 import { ROLE as USER_ROLE } from '../../common/user';
+import Button from '../../common/Button';
+import MembersList from '../common/MembersList';
 import NightCover from '../common/NightCover';
 
+import './style.less';
+
 export default class Mafia extends Component {
+
     render() {
         if (this.props.user.role === USER_ROLE.MAFIA) {
-            return <h1>Mafia Turn</h1>
+            return (
+                <div id='mafia'>
+                    <div className="title">{this.props.game.title}</div>
+                    <div className='hint'>Choose a victim!</div>
+                    <MembersList 
+                        members={this.props.game.members}/>
+                    <Button text='Accept' />
+                </div>
+            )
         } else {
             return <NightCover/>;
         }
