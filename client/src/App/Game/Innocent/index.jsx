46758555391e2209d6ef9b826386d4cf795d3fab
birthdaysgift <most-208@yandex.ru>
2021-01-24 16:35:10 +0300

    Add handling vote confirmation in Innocent

--- client/src/App/Game/Innocent/index.jsx
+++ client/src/App/Game/Innocent/index.jsx
@@ -5,8 +5,8 @@ import Dead from '../common/Dead';
 import InnocentWin from '../common/InnocentWin';
 import MafiaWin from '../common/MafiaWin';
 import MembersList from '../common/MembersList';
-import { STATE as USER_STATE } from '../../common/user';
-import game_proxy from '../../common/game';
+import { STATE as USER_STATE, ROLE as USER_ROLE } from '../../common/user';
+import game_proxy, { STATE as GAME_STATE } from '../../common/game';
 import socket from '../../common/socket';
 
 import audio from './city_awake.ogg';
@@ -25,6 +25,48 @@ export default class Innocent extends Component {
         if (this.props.user.id === this.props.game.hostID) {
             (new Audio(audio)).play();
         }
+    }
+
+    componentDidUpdate = () => {
+        let user = this.state.user;
+        let game = this.state.game;
+
+        if (user.id !== game.hostID) return;
+
+        for (const member of game.members.values()) {
+            if (member.state === USER_STATE.NOT_READY) return;
+        }
+
+        // runs only if all members are ready
+        let maxVotes = undefined;
+        game.members.forEach(member => {
+            if (maxVotes === undefined) {
+                maxVotes = member.id;
+                return;
+            }
+            if (member.state !== USER_STATE.DEAD) {
+                if (game.members.get(member.id).receivedVotes.size 
+                            > game.members.get(maxVotes).receivedVotes.size) {
+                    maxVotes = member.id;
+                }
+            }
+        });
+        if (maxVotes !== undefined) {
+            game.members.get(maxVotes).state = USER_STATE.DEAD;
+            if (game.members.get(maxVotes).role === USER_ROLE.MAFIA) {
+                game.mafiaAlive--;
+            } else {
+                game.innocentAlive--;
+            }
+        }
+        game.state = GAME_STATE.NIGHT;
+        game.members.forEach(member => {
+            if (member.state !== USER_STATE.DEAD) {
+                member.state = USER_STATE.NOT_READY;
+            }
+        });
+        game_proxy.object = game;
+        socket.emit('update', game_proxy.json);
     }
 
     highlightCondition = (member) => member.receivedVotes.has(this.state.user.id);
@@ -52,6 +94,18 @@ export default class Innocent extends Component {
         socket.emit('update', game_proxy.json);
     }
 
+    handleConfirmClick = () => {
+        let user = this.state.user;
+        let game = this.state.game;
+        if (game.members.get(user.id).state === USER_STATE.NOT_READY) {
+            game.members.get(user.id).state = USER_STATE.READY;
+        } else {
+            game.members.get(user.id).state = USER_STATE.NOT_READY;
+        }
+        game_proxy.object = game;
+        socket.emit('update', game_proxy.json);
+    };
+
     render() {
         if (this.props.game.innocentAlive === 0) return <MafiaWin/>
         if (this.props.game.mafiaAlive === 0) return <InnocentWin/>
@@ -67,7 +121,10 @@ export default class Innocent extends Component {
                     showCondition={this.showCondition}
                     highlightCondition={this.highlightCondition}
                     onMemberClick={this.handleMemberClick}/>
-                <Button text='Confirm' />
+                <Button
+                    text={(this.state.user.state === USER_STATE.NOT_READY) 
+                            ? 'Confirm' : 'Cancel'}
+                    onClick={this.handleConfirmClick}/>
             </div>
         )
     }
