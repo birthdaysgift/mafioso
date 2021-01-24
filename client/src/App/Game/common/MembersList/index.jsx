c76efe495377cb0bfe878dbc884237a5ed045751
birthdaysgift <most-208@yandex.ru>
2021-01-24 13:16:08 +0300

    Add showCondition to MembersList

--- client/src/App/Game/common/MembersList/index.jsx
+++ client/src/App/Game/common/MembersList/index.jsx
@@ -4,6 +4,8 @@ import img from './close50x50.png';
 import './style.less';
 
 export default class MembersList extends Component {
+    showCondition = (member) => this.props.showCondition?.(member) ?? true;
+
     render() {
         let closeIcon = (this.props.showCloseIcon)
                             ? <img className="img" src={img}/> : null;
@@ -13,12 +15,14 @@ export default class MembersList extends Component {
             let text = <div className="text">{member.name}</div>;
             let entryClass = this.props.highlightCondition?.(member)
                                 ? 'entry active' : 'entry';
-            members_elements.push(
-                <div className={entryClass} key={id} 
-                    onClick={e => this.props.onMemberClick?.(e,member)}> 
-                    {text} {closeIcon} 
-                </div>
-            );
+            if (this.showCondition(member)) {
+                members_elements.push(
+                    <div className={entryClass} key={id} 
+                        onClick={e => this.props.onMemberClick?.(e,member)}> 
+                        {text} {closeIcon} 
+                    </div>
+                );
+            }
         });
 
         return (
