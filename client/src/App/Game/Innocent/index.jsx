8619991b6ef98e3aae05a80c4245b4a32c3346a4
birthdaysgift <most-208@yandex.ru>
2021-01-24 13:20:01 +0300

    Add handleMemberClick to Innocent

--- client/src/App/Game/Innocent/index.jsx
+++ client/src/App/Game/Innocent/index.jsx
@@ -6,6 +6,8 @@ import InnocentWin from '../common/InnocentWin';
 import MafiaWin from '../common/MafiaWin';
 import MembersList from '../common/MembersList';
 import { STATE as USER_STATE } from '../../common/user';
+import game_proxy from '../../common/game';
+import socket from '../../common/socket';
 
 import audio from './city_awake.ogg';
 import './style.less';
@@ -25,6 +27,31 @@ export default class Innocent extends Component {
         }
     }
 
+    highlightCondition = (member) => member.receivedVotes.has(this.state.user.id);
+
+    showCondition = (member) => member.state !== USER_STATE.DEAD
+
+    handleMemberClick = (event, member) => {
+        let game = this.state.game;
+        let user = this.state.user;
+        let prevVote = user.vote;
+        let currVote = member.id;
+        if (prevVote === undefined) {
+            game.members.get(currVote).receivedVotes.add(user.id);
+            game.members.get(user.id).vote = currVote;
+        } else if (prevVote === currVote) {
+            game.members.get(currVote).receivedVotes.delete(user.id);
+            game.members.get(user.id).vote = undefined;
+        } else {
+            game.members.get(prevVote).receivedVotes.delete(user.id);
+            game.members.get(currVote).receivedVotes.add(user.id);
+            game.members.get(user.id).vote = currVote;
+        }
+
+        game_proxy.object = game;
+        socket.emit('update', game_proxy.json);
+    }
+
     render() {
         if (this.props.game.innocentAlive === 0) return <MafiaWin/>
         if (this.props.game.mafiaAlive === 0) return <InnocentWin/>
@@ -34,7 +61,11 @@ export default class Innocent extends Component {
             <div id="day">
                 <div className="title">{this.state.game.title}</div>
                 <div className="hint">Choose a suspect!</div>
-                <MembersList members={this.state.game.members} />
+                <MembersList 
+                    members={this.state.game.members} 
+                    showCondition={this.showCondition}
+                    highlightCondition={this.highlightCondition}
+                    onMemberClick={this.handleMemberClick}/>
                 <Button text='Confirm' />
             </div>
         )
